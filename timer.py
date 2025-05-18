bccd4568098b6c0ec0f027566292d0b23c55de45
miltoncarpenter665 <166653213+miltoncarpenter665@users.noreply.github.com>
2025-05-18 18:12:07 +0200

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import subprocess
+import json
+import os
+import time
+import os.path
+
+def zero_to_infinity():
+    i = 0
+    while True:
+        yield i
+        i += 1
+
+for y in zero_to_infinity():
+    print(time.strftime("%a, %d %b %Y %H:%M:%S"))
+    time.sleep(300)
+
